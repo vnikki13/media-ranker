11dc2a1e96454dbb5755e02bfb231c3e2150ca50
Nikki Vaughan <vnikki13@gmail.com>
2020-05-13 15:03:30 -0700

    able to update work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,4 +8,25 @@ class WorksController < ApplicationController
   def show
     @work = Work.find_by(id: params[:id])
   end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    elsif @work.update(work_params)
+      redirect_to work_path(@work)
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
