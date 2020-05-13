05304fc66875dc134de89c6ff8c19132ad8b5bad
Nikki Vaughan <vnikki13@gmail.com>
2020-05-13 15:28:44 -0700

    able to add new work and delete work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,15 +3,33 @@ class WorksController < ApplicationController
     @books = Work.where(category: 'book')
     @albums = Work.where(category: 'album')
     @movies = Work.where(category: 'movie')
-
   end
 
   def show
     @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_params(@work)
+    else
+      render :new
+    end
   end
 
   def edit
     @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
   end
 
   def update
@@ -19,7 +37,17 @@ class WorksController < ApplicationController
     if @work.update(work_params)
       redirect_to work_path(@work)
     else
-      render :new
+      render :edit
+    end
+  end
+
+  def destroy
+    work = Work.find_by(id: params[:id])
+    if work.nil?
+      head :not_found
+    else
+      work.destroy
+      redirect_to works_path
     end
   end
 
