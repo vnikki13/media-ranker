2617fa4d6147640459f6786c9b22b7adacc56027
Nikki Vaughan <vnikki13@gmail.com>
2020-05-13 17:05:22 -0700

    spotlight media and top 10catagories are randomly generated

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,2 +1,8 @@
 class HomepagesController < ApplicationController
+  def index
+    @spotlight = Work.top_work
+    @top_albums = Work.top_ten("album")
+    @top_books = Work.top_ten("book")
+    @top_movies = Work.top_ten("movie")
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,20 @@
 class Work < ApplicationRecord
+  
+  def self.top_work
+    num = rand(1..self.count)
+    return self.find_by(id: num)
+  end
+
+  def self.top_ten(category)
+    all_works = self.where(category: category)
+    top_works = []
+    if all_works.length > 10
+      10.times do
+        top_works << all_works.sample
+      end
+    else
+      return all_works
+    end
+    return top_works
+  end
 end
