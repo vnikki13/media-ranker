f7190b20be5285da301701aec676f6bff709dfcb
Nikki Vaughan <vnikki13@gmail.com>
2020-05-15 16:41:55 -0700

    added validation tests for Work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      @work = Work.new title: 'test title', publication_year: 4000, category: "test"
+    end
+
+    it 'is valid if required fields are present' do
+      result = @work.valid?
+      expect(result).must_equal true
+    end
+
+    it 'is invalid if missing a title' do
+      @work.title = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end
+
+    it 'is invalid if title apears twice in same category' do
+      new_work = Work.create title: 'test title', publication_year: 4000, category: "test"
+      result = new_work.valid?
+      expect(result).must_equal false
+    end
+
+    it 'is invalid if publication_year is missing' do
+      @work.publication_year = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end
+
+    it 'is invalid if publication_year is less than or more than 4 digits long' do
+      @work.publication_year = 345
+      first_result = @work.valid?
+      expect(first_result).must_equal false
+
+      @work.publication_year = 345383993
+      second_result = @work.valid?
+      expect(second_result).must_equal false
+    end
+  end
+
+  describe 'top_work' do
+    
+  end
+
+  describe 'top_ten' do
+    
+  end
 end
