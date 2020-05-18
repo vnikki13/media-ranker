81451ef0a7a0a648b24fb9d2605bde4be3433ba2
Nikki Vaughan <vnikki13@gmail.com>
2020-05-18 13:01:01 -0700

    Inital tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,13 +2,29 @@ require "test_helper"
 
 describe UsersController do
   it "must get login_form" do
-    get users_login_form_url
+    get login_path
     must_respond_with :success
   end
 
-  it "must get login" do
-    get users_login_url
-    must_respond_with :success
+  describe 'login' do
+    it "can log in a new user" do
+      user = nil
+      expect{
+        user = login()
+        }.must_differ 'User.count', 1
+      
+      must_respond_with :redirect
+      
+      expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal user_hash[:user][:username]
+    end
+
+    it 'can login an existing user' do
+      user = User.create(username: 'Nikki')
+      expect{login(user.username)}.wont_change 'User.count'
+      expect(session[:user_id]).must_equal user.id
+    end
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,11 +11,18 @@ Minitest::Reporters.use!(
 )
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  # parallelize(workers: :number_of_processors) # causes out of order output.
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def login(username = 'Nikki')
+    # Arrange
+    user_hash = {
+          user: {
+            username: username
+            }
+          }
+    
+    post login_path, params: user_hash
+    user = User.find_by(username: username)
+    return user
+  end
 end
