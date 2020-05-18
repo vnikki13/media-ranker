266c26c22832fc35ef51cb640422ec2cc5c8306b
Nikki Vaughan <vnikki13@gmail.com>
2020-05-18 14:24:02 -0700

    created login / logout link in nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,23 @@ class UsersController < ApplicationController
       flash[:welcome] = "Welcome back #{user.username}"
     end
 
-    session[:use_id] = user.id
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "Goodbye #{user.username}"
+      else
+        session[:user_id] = nil
+        flash[:notice] = "Error Unknown User"
+      end
+    else
+      flash[:error] = "You must be logged in to logout"
+    end
     redirect_to root_path
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,13 +17,22 @@ describe UsersController do
       
       expect(user).wont_be_nil
       expect(session[:user_id]).must_equal user.id
-      expect(user.username).must_equal user_hash[:user][:username]
+      expect(user.username).must_equal "Nikki"
     end
 
     it 'can login an existing user' do
       user = User.create(username: 'Nikki')
       expect{login(user.username)}.wont_change 'User.count'
       expect(session[:user_id]).must_equal user.id
+    end
+  end
+
+  describe 'logout' do
+    it 'can logout a logged in user' do
+      login()
+      expect(session[:user_id]).wont_be_nil
+      post logout_path
+      expect(session[:user_id]).must_be_nil
     end
   end
 
