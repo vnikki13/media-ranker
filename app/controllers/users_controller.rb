6a248c75baf4ff7520a81fd6c3e41aa2a1aae85e
Nikki Vaughan <vnikki13@gmail.com>
2020-05-18 14:09:25 -0700

    updated schema to now use a username for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def login
+    user = User.find_by(username: params[:user][:username])
+    if user.nil?
+      user = User.new(username: params[:user][:username])
+      if !user.save
+        flash[:error] = "unable to login"
+        redirect_to root_path
+        return
+      end
+      flash[:welcome] = "Welcome #{user.username}"
+    else
+      flash[:welcome] = "Welcome back #{user.username}"
+    end
+
+    session[:use_id] = user.id
+    redirect_to root_path
   end
 end
