dfd472e51bcdc5535faa8fc2b302f3a4df404781
Nikki Vaughan <vnikki13@gmail.com>
2020-05-22 15:59:04 -0700

    removed unused route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
-  get "/users/current", to: "users#current", as: "current_user"
   root 'homepages#index'
   resources :works
   resources :users, only: [:index, :show]
